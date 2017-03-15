4cd922a7b0dc2dc5e097c41d3980e02f37058d26
MariqAt <atanasova19@abv.bg>
2017-03-15 14:02:56 +0200

    add class OLX

---
+++ app/src/main/java/com/ittalents/myfirstaplication/model/OLX.java
@@ -0,0 +1,68 @@
+package com.ittalents.myfirstaplication.model;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Created by ASUS on 15.3.2017 г..
+ */
+
+public class OLX {
+
+
+    public static TreeSet<RegularUser> regularUsers;
+    // mail -> password -> user
+    // HashMap<String, TreeSet<RegularUser>> regularUsers;
+
+    public static TreeSet<RegularUser> loggedRegularUsers;
+
+    public static TreeMap<RegularUser.Category, ArrayList<RegularUser.Notice>> ads;
+
+    public static TreeMap<RegularUser.Category, TreeSet<RegularUser.Notice>> archivedAds;
+
+    private static OLX instance;
+
+    private OLX() {
+        super();
+
+        this.regularUsers = new TreeSet<RegularUser>();
+        this.loggedRegularUsers = new TreeSet<RegularUser>();
+        this.ads = new TreeMap<RegularUser.Category, ArrayList<RegularUser.Notice>>();
+        this.archivedAds = new TreeMap<RegularUser.Category, TreeSet<RegularUser.Notice>>();
+    }
+
+    void regUser(RegularUser user) {
+        this.regularUsers.add(user);
+    }
+
+    public void logInUser(String mail, String password) {
+        for (RegularUser regularUser : regularUsers) {
+            if (regularUser.getMail().equals(mail)) {
+                User user = (User) regularUser;
+                if (user.getPassword().equals(password)) {
+                    // System.out.println("Wolcome " + regularUser.getName());
+                    this.loggedRegularUsers.add(regularUser);
+                    return;
+                }
+            }
+        }
+        System.out.println("Your mail or your password aren't correct! Please, try again!");
+    }
+
+    public void logOutUser(RegularUser user) {
+        for (RegularUser regularUser : loggedRegularUsers) {
+            if (regularUser.equals(user)) {
+                this.loggedRegularUsers.remove(regularUser);
+            }
+        }
+    }
+
+    public static OLX getInstance() {
+        if (instance == null) {
+            instance = new OLX();
+        }
+        return instance;
+    }
+
+}
